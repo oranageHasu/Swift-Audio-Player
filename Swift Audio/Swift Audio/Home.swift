b0d278569c7832490e891dd2673e472595277923
oranageHasu <blair.petrachek@gmail.com>
2020-06-20 14:12:28 -0500

    Created project
    Created the initial iOS/SwiftUI project.  Added folders for resources,models, supporting components.

---
+++ Swift Audio/Swift Audio/Home.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Audio
+//
+//  Created by Blair Petrachek on 2020-06-20.
+//  Copyright © 2020 Blair Petrachek. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
