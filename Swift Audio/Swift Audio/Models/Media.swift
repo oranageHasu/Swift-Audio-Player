8bcf268c18a813e54d6819bb3c227c5dacc1ec32
oranageHasu <blair.petrachek@gmail.com>
2020-06-20 15:19:33 -0500

    Added Media* model
    Added the Media model as well as a json data file to stub in sample input for early development.

---
+++ Swift Audio/Swift Audio/Models/Media.swift
@@ -0,0 +1,35 @@
+//
+//  Media.swift
+//  Swift Audio
+//
+//  Created by Blair Petrachek on 2020-06-20.
+//  Copyright © 2020 Blair Petrachek. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Media: Hashable, Codable, Identifiable {
+    
+    // id of the Media within the User's library
+    var id: Int
+    
+    // Media Meta Data
+    var artist: String
+    var title: String
+    
+    // Media source
+    var source: Source
+    
+    // User Preferences
+    var isFavorite: Bool
+    
+    // Owner Preferences
+    var isFeatured: Bool
+
+    // Enum registering supported input streams for media
+    enum Source: String, CaseIterable, Codable, Hashable {
+        case digitalFile = "DigitalFile"
+        case youtube = "Youtube"
+    }
+}
